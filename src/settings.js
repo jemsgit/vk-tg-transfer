5a60e020e173a466d4040b9158b6381664afe0b5
jem jem <jemguns@gmail.com>
2024-12-24 11:55:19 +0400

    change tags

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,16 @@
 const settings = {
   77655964: {
-    tgChannel: '@club_nycoffee',
-    tag: 'афиша_nycoffee'
+    tgChannel: "@club_nycoffee",
+    tag: "share",
   },
   42471820: {
-    tgChannel: '@druzhba_saratov',
-    tag: 'афиша_дружба'
+    tgChannel: "@druzhba_saratov",
+    tag: "share",
   },
-  219135986 : {
-    tgChannel: '@musicopenmic64',
-    tag: 'share'
-  }
-}
+  219135986: {
+    tgChannel: "@musicopenmic64",
+    tag: "share",
+  },
+};
 
 module.exports = settings;
